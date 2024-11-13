da29daee1eaebabf306d37797ff30316c3cf9c18
Jonne Roponen <Jonne.Roponen@metropolia.fi>
2024-11-13 12:31:43 +0200

    Initial commit

---
+++ vite.config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
+export default defineConfig({
+  base: "/coding-marathon1-group6/",
+  plugins: [react()],
+})
